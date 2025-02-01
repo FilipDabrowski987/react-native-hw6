d4a1544c583dcc4fcb3d950ebdca46b8bb939c70
FilipDabrowski987 <filipdabrowski@vp.pl>
2025-02-01 15:25:46 +0100

    update

--- app/(tabs)/save-data.tsx
+++ app/(tabs)/save-data.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import * as Location from 'expo-location';
 import { CameraView, useCameraPermissions } from 'expo-camera';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SaveDataScreen() {
 
@@ -43,24 +44,41 @@ export default function SaveDataScreen() {
         <View/>
     }
 
-    if (!permission.granted) {
+    if (!permission?.granted) {
         return (
+            <SafeAreaView style={{ flex: 1 }}>
             <View>
-            <Text>Nie udzielono uprawnień dostępu do aparatu</Text>
-                <Button title='Udziel uprawnienia' onPress={getPermission} />
-            </View>
+            <Text style={styles.text}>Nie udzielono uprawnień dostępu do aparatu</Text>
+                <Button title='Udziel dostępu' onPress={getPermission} />
+                </View>
+            </SafeAreaView>
         )
     }
 
-  return (
-    <View style={styles.container}>
-          <Text style={styles.title}>Lokalizacja GPS</Text>
-          <Button title='Pobierz lokalizację' onPress={getLocation} />
-          {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
-          {localization && (
-              <Text>Współrzędne: {localization.latitude}, {localization.longitude}</Text>)}
-    </View>
-    );
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+        <View>                
+                <Text style={styles.title}>Lokalizacja GPS</Text>
+                <Button title='Pobierz lokalizację' onPress={getLocation} />
+                {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
+                {localization && (
+                    <Text>Współrzędne: {localization.latitude}, {localization.longitude}</Text>)}
+            </View>
+            <View>
+                <View style={styles.cameraContainer}>
+                    <CameraView style={styles.camera} ref={cameraRef} />
+                </View>
+                <View>
+                    <Button title='Zrób zdjęcie' onPress={async () => {
+                        const photo = await cameraRef.current?.takePictureAsync()
+                        alert(`Zdjęcie zrobiono w wymiarach: ${photo.width}x${photo?.height}`)
+                    }} />
+                </View>
+            </View>             
+        </View>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -70,23 +88,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'lightgray',
     padding: 20,
-    gap: 20,
-  },
-  title: {
+        gap: 20,
+ },
+    title: {
     fontSize: 20,
     fontWeight: "700",
-  },
-  input: {
-    height: 40,
-    width: '80%',
-    borderWidth: 1,
-    borderColor: "red",
-    borderRadius: 20,
-    padding: 10,
-    backgroundColor: "white",
   },
   error: {
     color: "red",
     marginBottom: 10,
-  }
+    },
+  text: {
+    color: "white"
+    },
+    camera: {
+    flex: 1,
+    borderRadius: 10,
+    backgroundColor: 'gray'
+  },
+    cameraContainer: {
+    width: 300,
+    height: 300,
+    marginTop: 50,
+  },
 })
